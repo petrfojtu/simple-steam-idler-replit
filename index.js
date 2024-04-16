e7a3a611cabf4fd363924c00f7d8a4b728a4a46f
petanfh <150440652+petrfojtu@users.noreply.github.com>
2024-04-16 19:05:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ user.on('loggedOn', () => {
 	user.setPersona(status);               
 	user.gamesPlayed(games);
 });
+
+
+var username2 = process.env.username;
+var password2 = process.env.password;
+var shared_secret2 = process.env.shared;
+
+var games2 = [730, 570];  // Enter here AppIDs of the needed games CS2,Dota2
+var status2 = 1;  // 1 - online, 7 - invisible
+
+
+user2 = new steamUser();
+user2.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user2.on('loggedOn', () => {
+	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
+	user.setPersona(status);               
+	user.gamesPlayed(games);
+});
